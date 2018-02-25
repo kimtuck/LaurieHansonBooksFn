9a77cf5d7ed21c54ec279ad35cf956804e37ce17
A477MZZ <robert.hanson@solutiondesign.com>
2018-02-24 18:40:38 -0600

    example

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,3 +1,5 @@
+/*https://sendgrid.com/docs/Classroom/Send/v3_Mail_Send/personalizations.html*/
+
 module.exports = function (context, input) {
 
     var extend = function() {
@@ -19,37 +21,17 @@ module.exports = function (context, input) {
 
     var message = {
         "personalizations": [
-            { "to": [
-                { "email": "robert.hanson@solutiondesign.com" },
-                {"-name-": ["john"]}
-                ]
+            {
+                "to": [{"email": "robert.hanson@solutiondesign.com"}],
+                "substitutions": {
+                    "-name-": 'thisIsMyName'
+                }
             }
         ],
-        "sub": {
-            "-name-": [
-                "John",
-                "Jane"
-            ],
-            "-customerID-": [
-                "1234",
-                "5678"
-            ],
-            "-salesContact-": [
-                "Jared",
-                "Ben"
-            ],
-            "-contactPhoneNumber-": [
-                "555.555.5555",
-                "777.777.7777"
-            ],
-            "-time-": [
-                "3:00pm",
-                "5:15pm"
-            ]
-        },
         from: { email: "sender@contoso.com" },
         subject: "Azure news",
-        template_id: '2675ca4b-a347-4bd3-8891-54b1270e2ef2'
+        template_id: '2675ca4b-a347-4bd3-8891-54b1270e2ef2',
+        "-name-": [ "mynameishello" ]
     };
     message = extend(message, parameters);
     context.log(message);
